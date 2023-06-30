11b29fb568b6c65b74efecd2e862f2c6e1323b69
Claudia <135162896+claudiad4@users.noreply.github.com>
2023-06-30 17:05:39 +0200

    Add files via upload

---
+++ Zadanie_1/Zadanie_1/Program.cs
@@ -0,0 +1,55 @@
+﻿public enum DzienTygodnia
+{
+    Poniedziałek,
+    Wtorek,
+    Środa,
+    Czwartek,
+    Piątek,
+    Sobota,
+    Niedziela
+}
+
+public enum EtapPrania
+{
+    Płukanie,
+    Pranie,
+    Odwirowanie,
+}
+
+public enum Posilek
+{
+    Śniadanie,
+    Lunch,
+    Obiad,
+    Podwieczorek,
+    Kolacja
+}
+
+public enum BierkiSzachowe
+{
+    Król,
+    Królowa,
+    Wieża,
+    Goniec,
+    Koń,
+    Pionek
+}
+
+class Program
+{
+    //Wypisz po jednym elemencie
+    static void Main(string[] args)
+    {
+        DzienTygodnia dzien = DzienTygodnia.Poniedziałek;
+        Console.WriteLine(dzien);
+
+        EtapPrania etap = EtapPrania.Odwirowanie;
+        Console.WriteLine(etap);
+
+        Posilek posilek = Posilek.Lunch;
+        Console.WriteLine(posilek);
+
+        BierkiSzachowe bierka = BierkiSzachowe.Koń;
+        Console.WriteLine(bierka);
+    }
+}
---
+++ Zadanie_2/Zadanie_2/Program.cs
@@ -0,0 +1,57 @@
+﻿public struct Student
+{
+    public string Imię;
+    public string Nazwisko;
+    public string Kierunek;
+    public int Rok;
+    public string Stopień;
+}
+
+public struct Przedmiot
+{
+    public string Nazwa;
+    public string Prowadzący;
+    public string Kierunek;
+}
+
+public struct NauczycielAkademicki
+{
+    public string TytułNaukowy;
+    public string Imię;
+    public string Nazwisko;
+    public string Wydział;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student
+        {
+            Imię = "Jan",
+            Nazwisko = "Kowalski",
+            Kierunek = "Informatyka",
+            Rok = 2,
+            Stopień = "Licencjat"
+        };
+
+        Przedmiot przedmiot = new Przedmiot
+        {
+            Nazwa = "Programowanie obiektowe",
+            Prowadzący = "Dr Adam Nowak",
+            Kierunek = "Informatyka"
+        };
+
+        NauczycielAkademicki nauczyciel = new NauczycielAkademicki
+        {
+            TytułNaukowy = "Dr inż.",
+            Imię = "Justyna",
+            Nazwisko = "Kowalska",
+            Wydział = "Wydział Informatyki"
+        };
+
+        Console.WriteLine("Student: " + student.Imię + " " + student.Nazwisko);
+        Console.WriteLine("Przedmiot: " + przedmiot.Nazwa + ", Prowadzący: " + przedmiot.Prowadzący);
+        Console.WriteLine("Nauczyciel: " + nauczyciel.TytułNaukowy + " " + nauczyciel.Imię + " " + nauczyciel.Nazwisko);
+    }
+}
